71685254203a782791e16b9cdd4dfa8a3ad3d865
thalesgelinger <thalesgelinger@gmail.com>
2021-02-26 11:31:52 -0300

    feat: config server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import cors from 'cors';
 import routes from './routes';
+
+const PORT = 3333;
 
 const app = express();
 
-app.use(cors());
 app.use(routes);
 
-app.listen(3333);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ message: 'ok' }));
+routes.get('/', (req, res) => {
+  res.json({ message: 'ok' });
+});
 
 export default routes;
